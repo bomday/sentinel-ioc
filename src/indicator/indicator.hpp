c2403842cd075315265e687a763c4a3adfee23d6
Day Lima <dayanecamilelima@gmail.com>
2025-06-16 22:44:42 -0300

    getters, setters and virtual destructor

--- src/indicator/indicator.hpp
+++ src/indicator/indicator.hpp
@@ -3,6 +3,7 @@
 #ifndef INDICATOR_HPP
 #define INDICATOR_HPP
 #include <string>
+#include <vector>
 
 class Indicator {
     private:
@@ -17,6 +18,25 @@ class Indicator {
     public:
         // Constructor
         Indicator(int indicatorId, int severity, std::string type, std::string description, std::string origin, std::string timestamp);
+        
+        // Virtual destructor
+        virtual ~Indicator() = default;
+
+        // Getters
+        int getIndicatorId() const { return indicatorId; }
+        int getSeverity() const { return severity; }
+        std::string getType() const { return type; }
+        std::string getDescription() const { return description; }
+        std::string getOrigin() const { return origin; }
+        std::string getTimestamp() const { return timestamp; }
+
+        // Setters
+        void setSeverity(int sev) { severity = sev; }    
+        void setType(const std::string& t) { type = t; }
+        void setDescription(const std::string& desc) { description = desc; }
+        void setOrigin(const std::string& orig) { origin = orig; }
+        void setTimestamp(const std::string& ts) { timestamp = ts; }
+        
 };
 
 #endif
--- src/maliciousHash/maliciousHash.hpp
+++ src/maliciousHash/maliciousHash.hpp
@@ -16,7 +16,19 @@ class MaliciousHash : public Indicator {
         // Constructor
         MaliciousHash(int indicatorId, int severity, std::string type, std::string description, std::string origin, std::string timestamp, std::string hash, std::string algorithm);
 
-    static Indicator* createMaliciousHash(int indicatorId, int severity, std::string type, std::string description, std::string origin, std::string timestamp);
+        // Function to create an indicator of type MaliciousHash
+        static Indicator* createMaliciousHash(int indicatorId, int severity, std::string type, std::string description, std::string origin, std::string timestamp);
+
+        // Virtual destructor
+        virtual ~MaliciousHash() = default;
+
+        // Getters
+        std::string getHash() const { return hash; }
+        std::string getAlgorithm() const { return algorithm; }
+        
+        // Setters
+        void setHash(const std::string& hashValue) { hash = hashValue; }
+        void setAlgorithm(const std::string& algorithmName) { algorithm = algorithmName; }
 };
 
 #endif
