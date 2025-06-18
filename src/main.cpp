c60220cf0bdea360b3fae2a99b541264928d5d13
Day Lima <dayanecamilelima@gmail.com>
2025-06-18 00:38:34 -0300

    code refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -19,21 +19,17 @@ int main() {
         showMenu(); // Call showMenu function to display the menu
         std::cout << "Enter your option: ";
         std::cin >> optionNumber;
-        // Important: Clear the input buffer after reading an integer,
-        // especially if you're mixing std::cin >> var with std::getline later.
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
         switch (optionNumber) {
             case 1:
-                // When creating an indicator, ensure createIndicator() returns a dynamically
-                // allocated Indicator* that AnalyzerIOC can manage.
-                { // Use a block to declare a local variable `newIoc`
+                { 
                     Indicator* newIoc = createIndicator(); // Call the function to create an indicator
                 }
                 break;
             case 2:
                 // Implement Listing IOCs here
-                listIndicators(); // Call the listIndicators method from your analyzer object
+                listIndicators(); 
                 break;
             case 3:
                 // Edit IOC (placeholder)
@@ -51,9 +47,6 @@ int main() {
         }
         std::cout << "\n"; // Add a newline for better spacing after each operation
     } while (optionNumber != 0);
-
-    // The iocAnalyzer object will automatically call its destructor when main() exits,
-    // which handles the deallocation of all Indicator objects.
 
     return 0;
 }
